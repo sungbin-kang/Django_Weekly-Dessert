4e79db71df71325d98ce5d3a975d844ff701a9de
Sungbin_Kang <sungbin9353@gmail.com>
2021-11-23 11:38:53 -0800

    models, urls, views initial skelaton added, server not working - query set needs to be added weeklydessert.models.Week.DoesNotExist: Week matching query does not exist.

--- cafeteria/weeklydessert/urls.py
+++ cafeteria/weeklydessert/urls.py
@@ -1,5 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+
+from . import views
 
 urlpatterns = [
-    
+	path("", views.index, name="index"),
+  # Add a path to your auth links below:
+  
+	# Add a path to your signup request below:
+
+	# Add a path to your logout request below:
+
+	path("polls/<int:pk>/", views.DetailsView.as_view(), name="detail"),
+	path("polls/<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+	path("polls/<int:week_id>/vote/", views.vote, name="vote"),
 ]
--- cafeteria/weeklydessert/views.py
+++ cafeteria/weeklydessert/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import Http404
+from django.urls import reverse
+from .models import Week, Choice
+# import the login_required decorator and the LoginRequiredMixin mixin below
 
-# Create your views here.
+
+# Import the logout function from django.contrib.auth below
+
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+
+# Add login_required decorator:
+def index(request):
+  latest_week_poll = Week.objects.get(pk=1)
+  context = {
+     "name": request.user,
+    "week_poll": latest_week_poll
+  }
+  return render(request, "index.html", context)
+
+# Create your class-based Signup view below:
+
+class DetailsView(DetailView):
+  model = Week
+  template_name = "detail.html"
+
+class ResultsView(DetailView):
+  model = Week
+  template_name = "results.html"
+
+# Create your logout function, logout_request, below:
+
+# Add login_required decorator:
+def vote(request, week_id):
+  try:
+    week = Week.objects.get(pk=week_id)
+    selected_choice = week.choice_set.get(pk=request.POST["choice"])
+  except Week.DoesNotExist:
+    raise Http404("Poll for said week does not exists")
+  else:
+    selected_choice.votes += 1
+    selected_choice.save()
+    return redirect("results", week_id)
