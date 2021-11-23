61655d9c0ada20eb5420b9c3d77cb6308e57bdac
Sungbin_Kang <sungbin9353@gmail.com>
2021-11-23 13:00:42 -0800

    working on login required page

--- cafeteria/weeklydessert/urls.py
+++ cafeteria/weeklydessert/urls.py
@@ -5,11 +5,8 @@ from . import views
 urlpatterns = [
 	path("", views.index, name="index"),
     path("account/", include("django.contrib.auth.urls")),
-  
 	path("signup/", views.SignUp.as_view(), name="signup"),
-
 	path("logout/", views.logout_request, name="logout"),
-
 	path("polls/<int:pk>/", views.DetailsView.as_view(), name="detail"),
 	path("polls/<int:pk>/results/", views.ResultsView.as_view(), name="results"),
 	path("polls/<int:week_id>/vote/", views.vote, name="vote"),
